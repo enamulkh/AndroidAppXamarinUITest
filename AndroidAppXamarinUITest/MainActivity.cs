1357e771d2f6be17df2c63f60aeea01ea3cc4291
enamulkh <33300969+enamulkh@users.noreply.github.com>
2018-02-03 21:42:34 -0600

    Build afer .apk signing

--- AndroidAppXamarinUITest/MainActivity.cs
+++ AndroidAppXamarinUITest/MainActivity.cs
@@ -1,6 +1,12 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Distribute;
+using Microsoft.AppCenter.Push;
 
 namespace AndroidAppXamarinUITest
 {
@@ -13,6 +19,19 @@ namespace AndroidAppXamarinUITest
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            AppCenter.Start("2a70aa9d-ed34-4e01-8def-9ac5852e4c5d",
+                       typeof(Analytics), typeof(Crashes));
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+
+            button.Click += Button_Click;
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            EditText textEntry = FindViewById<EditText>(Resource.Id.textView1);
+            textEntry.Text = "Button Click is working";
         }
     }
 }
